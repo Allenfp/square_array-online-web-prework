6ccc73e4d8c042d7d2b3e37be1557345c2ff7827
Allen F Partlow <allenfp@gmail.com>
2019-01-24 06:30:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  new_array = []
-  array.each |element|
-    new_array << element * element
-
+  # new_array = []
+  # array.each |element|
+  #   new_array << element * element
+  #
 end
